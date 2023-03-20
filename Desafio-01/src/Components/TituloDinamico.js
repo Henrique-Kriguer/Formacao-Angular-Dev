9c6162fadbc256d4570a9035684c2d3ab42f189c
Henrique Kriguer <henriquekriguer@gmail.com>
2023-03-20 19:35:19 -0300

    criando shadow dom

---
+++ Desafio-01/src/Components/TituloDinamico.js
@@ -0,0 +1,27 @@
+class TituloDinamico extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this .attachShadow({ mode: "open"});
+        // base do componente
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = 'Henrique'
+
+        // estilizar componente
+        const style = document.createElement("style");
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `
+
+
+        // enviar para shadow
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+
+    }
+}
+
+customElements.define('titulo-dinamico',TituloDinamico);
